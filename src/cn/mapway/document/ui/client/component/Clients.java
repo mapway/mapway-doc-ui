9b791444ee1c8d655c017bae69a0588588df22f9
zhangjianshe <zhangjsf@enn.cn>
2016-12-05 15:41:09 +0800

    修正版本号

---
+++ src/cn/mapway/document/ui/client/component/Clients.java
@@ -0,0 +1,24 @@
+package cn.mapway.document.ui.client.component;
+
+import com.google.gwt.core.shared.GWT;
+import com.google.gwt.user.client.Window;
+
+
+/**
+ * The Class Clients.
+ */
+public class Clients {
+
+	/**
+	 * 获取主机网络地址.
+	 *
+	 * @return the host port
+	 */
+	public static final String getHostPort() {
+		String host = Window.Location.getHost();
+		String protocol = Window.Location.getProtocol();
+		String r = protocol + "//" + host;
+		GWT.log(r);
+		return r;
+	}
+}
