5d737e43ef303207cb3734e5e96b95c0a546a861
zhangjsf <zhangjsf@enn.cn>
2017-08-21 11:50:49 +0800

    index.html 增加参数 withTop

--- src/cn/mapway/document/parser/CompileVersion.java
+++ src/cn/mapway/document/parser/CompileVersion.java
@@ -1,4 +1,4 @@
 package cn.mapway.document.parser;
 public class  CompileVersion {
-public final static String  VERSION =   "[1.0.41-SNAPSHOT]@2017年08月09日 17:08:01";
+public final static String  VERSION =   "[1.0.46-SNAPSHOT]@2017年08月18日 14:50:08";
 }
--- src/cn/mapway/document/ui/client/main/MainFrame.java
+++ src/cn/mapway/document/ui/client/main/MainFrame.java
@@ -20,6 +20,7 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
@@ -34,241 +35,252 @@ import com.google.gwt.user.client.ui.Widget;
  */
 public class MainFrame extends Composite {
 
-	/** The ui binder. */
-	private static MainFrameUiBinder uiBinder = GWT
-			.create(MainFrameUiBinder.class);
+  /** The ui binder. */
+  private static MainFrameUiBinder uiBinder = GWT.create(MainFrameUiBinder.class);
 
-	/**
-	 * The Interface MainFrameUiBinder.
-	 */
-	interface MainFrameUiBinder extends UiBinder<Widget, MainFrame> {
-	}
+  /**
+   * The Interface MainFrameUiBinder.
+   */
+  interface MainFrameUiBinder extends UiBinder<Widget, MainFrame> {
+  }
 
-	/** The current item. */
-	TreeItem currentItem = null;
+  /** The current item. */
+  TreeItem currentItem = null;
 
-	/** The entry panel. */
-	EntryPanel entryPanel;
+  /** The entry panel. */
+  EntryPanel entryPanel;
 
-	/** The tree select. */
-	private SelectionHandler<TreeItem> treeSelect = new SelectionHandler<TreeItem>() {
+  /** The tree select. */
+  private SelectionHandler<TreeItem> treeSelect = new SelectionHandler<TreeItem>() {
 
-		@Override
-		public void onSelection(SelectionEvent<TreeItem> arg0) {
-			TreeItem item = arg0.getSelectedItem();
-			if (currentItem != null) {
-				currentItem.removeStyleName(SysResource.INSTANCE.getCss()
-						.entrySelected());
-			}
+    @Override
+    public void onSelection(SelectionEvent<TreeItem> arg0) {
+      TreeItem item = arg0.getSelectedItem();
+      if (currentItem != null) {
+        currentItem.removeStyleName(SysResource.INSTANCE.getCss().entrySelected());
+      }
 
-			handleItem(item);
+      handleItem(item);
 
-			currentItem = item;
-			currentItem.addStyleName(SysResource.INSTANCE.getCss()
-					.entrySelected());
-		}
+      currentItem = item;
+      currentItem.addStyleName(SysResource.INSTANCE.getCss().entrySelected());
+    }
 
-	};
+  };
 
-	/**
-	 * Handle item.
-	 *
-	 * @param item
-	 *            the item
-	 */
-	private void handleItem(TreeItem item) {
-		if (item.getTitle().length() == 0) {
-			Group group = (Group) item.getUserObject();
-			showEntryList(group);
-		} else {
-			Entry e = (Entry) item.getUserObject();
-			showEntry(e);
-		}
+  /**
+   * Handle item.
+   *
+   * @param item the item
+   */
+  private void handleItem(TreeItem item) {
+    if (item.getTitle().length() == 0) {
+      Group group = (Group) item.getUserObject();
+      showEntryList(group);
+    } else {
+      Entry e = (Entry) item.getUserObject();
+      showEntry(e);
+    }
 
-	}
+  }
 
-	/**
-	 * Show entry.
-	 *
-	 * @param e
-	 *            the e
-	 */
-	private void showEntry(Entry e) {
-		if (entryPanel == null) {
-			entryPanel = new EntryPanel();
+  /**
+   * Show entry.
+   *
+   * @param e the e
+   */
+  private void showEntry(Entry e) {
+    if (entryPanel == null) {
+      entryPanel = new EntryPanel();
 
-		}
-		if (!entryPanel.isAttached()) {
-			content.clear();
-			content.add(entryPanel);
-		}
-		entryPanel.parse(e);
-		content.scrollToTop();
+    }
+    if (!entryPanel.isAttached()) {
+      content.clear();
+      content.add(entryPanel);
+    }
+    entryPanel.parse(e);
+    content.scrollToTop();
 
-	}
+  }
 
-	/** The list. */
-	EntryListPanel list;
+  /** The list. */
+  EntryListPanel list;
 
-	/**
-	 * Show entry list.
-	 *
-	 * @param group
-	 *            the group
-	 */
-	protected void showEntryList(Group group) {
-		if (list == null) {
-			list = new EntryListPanel();
-		}
-		if (!list.isAttached()) {
-			content.clear();
-			content.add(list);
-		}
-		list.parse(group, "");
-		content.scrollToTop();
-	}
+  /**
+   * Show entry list.
+   *
+   * @param group the group
+   */
+  protected void showEntryList(Group group) {
+    if (list == null) {
+      list = new EntryListPanel();
+    }
+    if (!list.isAttached()) {
+      content.clear();
+      content.add(list);
+    }
+    list.parse(group, "");
+    content.scrollToTop();
+  }
 
-	/**
-	 * Instantiates a new main frame.
-	 */
-	public MainFrame() {
-		initWidget(uiBinder.createAndBindUi(this));
-		logo.setUrl(SysResource.INSTANCE.logo().getSafeUri());
+  /**
+   * Instantiates a new main frame.
+   */
+  public MainFrame() {
+    initWidget(uiBinder.createAndBindUi(this));
+    logo.setUrl(SysResource.INSTANCE.logo().getSafeUri());
 
-		apiTree.addSelectionHandler(treeSelect);
-	}
+    apiTree.addSelectionHandler(treeSelect);
 
-	/** The doc. */
-	ApiDoc doc;
+    adjustUI();
+  }
 
-	/** The goto word handler. */
-	private ClickHandler gotoWordHandler = new ClickHandler() {
+  /**
+   * 根据参数调整UI布局
+   */
+  private void adjustUI() {
+    String showTop = Window.Location.getParameter("showTop");
+    if (showTop == null || showTop.length() == 0) {
+      // 显示TOP
+    } else {
+      // 不显示TOP
+      root.setWidgetHidden(topBar, true);
+    }
+  }
 
-		@Override
-		public void onClick(ClickEvent event) {
-			Window.open(doc.wordUrl(), "wordExport", "");
-		}
-	};
+  /** The doc. */
+  ApiDoc doc;
 
-	/** The download link handler. */
-	private ClickHandler downloadLinkHandler = new ClickHandler() {
+  /** The goto word handler. */
+  private ClickHandler gotoWordHandler = new ClickHandler() {
 
-		@Override
-		public void onClick(ClickEvent event) {
-			CustomButton btn = (CustomButton) event.getSource();
-			JarInfo jar = (JarInfo) btn.getData();
-			Window.open(jar.link(), "Connecgtor 下载", "");
-		}
-	};
+    @Override
+    public void onClick(ClickEvent event) {
+      Window.open(doc.wordUrl(), "wordExport", "");
+    }
+  };
 
-	/**
-	 * 获取模板文件中的数据.
-	 *
-	 * @return the api doc
-	 */
-	private final native ApiDoc findDocData()/*-{
+  /** The download link handler. */
+  private ClickHandler downloadLinkHandler = new ClickHandler() {
+
+    @Override
+    public void onClick(ClickEvent event) {
+      CustomButton btn = (CustomButton) event.getSource();
+      JarInfo jar = (JarInfo) btn.getData();
+      Window.open(jar.link(), "Connecgtor 下载", "");
+    }
+  };
+
+  /**
+   * 获取模板文件中的数据.
+   *
+   * @return the api doc
+   */
+  private final native ApiDoc findDocData()/*-{
 		if ($wnd.g_data != undefined) {
 			return $wnd.g_data;
 		}
 		return null;
-	}-*/;
+  }-*/;
 
-	/**
-	 * Inits the.
-	 *
-	 * @param target
-	 *            the target
-	 */
-	public void init(String target) {
+  /**
+   * Inits the.
+   *
+   * @param target the target
+   */
+  public void init(String target) {
 
-		ApiDoc doc = findDocData();
-		if (doc == null) {
-			ApiDocProxy.fetchData(target,
-					new IOnData<cn.mapway.document.ui.client.module.ApiDoc>() {
-						@Override
-						public void onError(String url, String error) {
-							Window.alert(error);
-						}
+    ApiDoc doc = findDocData();
+    if (doc == null) {
+      ApiDocProxy.fetchData(target, new IOnData<cn.mapway.document.ui.client.module.ApiDoc>() {
+        @Override
+        public void onError(String url, String error) {
+          Window.alert(error);
+        }
 
-						@Override
-						public void onSuccess(String url,
-								cn.mapway.document.ui.client.module.ApiDoc data) {
-							parseData(data);
-						}
-					});
-		} else {
-			parseData(doc);
-		}
-	}
+        @Override
+        public void onSuccess(String url, cn.mapway.document.ui.client.module.ApiDoc data) {
+          parseData(data);
+        }
+      });
+    } else {
+      parseData(doc);
+    }
+  }
 
-	/**
-	 * Parses the data.
-	 *
-	 * @param doc
-	 *            the doc
-	 */
-	void parseData(ApiDoc doc) {
-		this.doc = doc;
+  /**
+   * Parses the data.
+   *
+   * @param doc the doc
+   */
+  void parseData(ApiDoc doc) {
+    this.doc = doc;
 
-		JsArray<JarInfo> jars = doc.getDownloads();
+    JsArray<JarInfo> jars = doc.getDownloads();
 
-		if (jars.length() > 0) {
-			for (int index = 0; index < jars.length(); index++) {
-				JarInfo jar = jars.get(index);
-				CustomButton btn = new CustomButton(jar.summary());
-				btn.setData(jar);
-				btn.addClickHandler(downloadLinkHandler);
-				tools.add(btn);
-			}
-		}
+    if (jars.length() > 0) {
+      for (int index = 0; index < jars.length(); index++) {
+        JarInfo jar = jars.get(index);
+        CustomButton btn = new CustomButton(jar.summary());
+        btn.setData(jar);
+        btn.addClickHandler(downloadLinkHandler);
+        tools.add(btn);
+      }
+    }
 
-		lbTitle.setText(doc.title());
-		apiTree.parseData(doc);
-		lbSubtitle.setText(doc.summary());
-		handleItem(apiTree.getItem(0));
+    lbTitle.setText(doc.title());
+    apiTree.parseData(doc);
+    lbSubtitle.setText(doc.summary());
+    handleItem(apiTree.getItem(0));
 
 
-	
-		if (doc.domain() != null) {
-			anchorDomain.setText(doc.domain());
-		}
 
-		if (doc.copyright() != null) {
-			lbCopy.setHTML("&copy; " + doc.copyright());
-		}
+    if (doc.domain() != null) {
+      anchorDomain.setText(doc.domain());
+    }
 
-	}
+    if (doc.copyright() != null) {
+      lbCopy.setHTML("&copy; " + doc.copyright());
+    }
 
-	/** The api tree. */
-	@UiField
-	ApiTree apiTree;
+  }
 
-	/** The content. */
-	@UiField
-	ScrollPanel content;
+  /** The api tree. */
+  @UiField
+  ApiTree apiTree;
 
-	/** The lb title. */
-	@UiField
-	Label lbTitle;
+  /** The content. */
+  @UiField
+  ScrollPanel content;
 
-	/** The logo. */
-	@UiField
-	Image logo;
+  /** The lb title. */
+  @UiField
+  Label lbTitle;
 
-	/** The tools. */
-	@UiField
-	HorizontalPanel tools;
+  /** The logo. */
+  @UiField
+  Image logo;
 
-	/** sub title. */
-	@UiField
-	Label lbSubtitle;
+  /** The tools. */
+  @UiField
+  HorizontalPanel tools;
 
-	/** sub title. */
-	@UiField
-	HTML lbCopy;
+  /** sub title. */
+  @UiField
+  Label lbSubtitle;
 
-	/** sub title. */
-	@UiField
-	Anchor anchorDomain;
+  /** sub title. */
+  @UiField
+  HTML lbCopy;
+
+  /** sub title. */
+  @UiField
+  Anchor anchorDomain;
+
+  @UiField
+  HorizontalPanel topBar;
+
+  @UiField
+  DockLayoutPanel root;
 
 }
