d55284a4ca7948e4d44c5812d26524e1f7c6cb93
zhangjsf <zhangjsf@enn.cn>
2017-08-25 11:35:48 +0800

    Add compile file information

--- src/cn/mapway/document/parser/CompileVersion.java
+++ src/cn/mapway/document/parser/CompileVersion.java
@@ -1,4 +1,4 @@
 package cn.mapway.document.parser;
 public class  CompileVersion {
-public final static String  VERSION =   "[1.0.46-SNAPSHOT]@2017年08月18日 14:50:08";
+public final static String  VERSION =   "[1.0.48-SNAPSHOT]@2017年08月24日 15:59:21";
 }
